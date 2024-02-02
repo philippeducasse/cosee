da010891f270c33a2a3a52ed712b0d449b8ee152
philo <ducassephi@hotmail.fr>
2024-02-02 12:00:41 +0100

    changed fonts

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+// allows modification or extension of next.js configuration (add webpacks, enable compression, define env variables...)
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 }
