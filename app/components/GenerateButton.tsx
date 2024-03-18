9408b92f526281540e83677d46fb79787aac4d58
philo <ducassephi@hotmail.fr>
2024-03-18 11:47:40 +0100

    updated edenAI

--- app/components/GenerateButton.tsx
+++ app/components/GenerateButton.tsx
@@ -1,66 +1,78 @@
-import React from 'react';
-import { GenerateButtonProps } from '../page';
+import React from "react";
+import { GenerateButtonProps } from "../page";
 
 // Ensure you've imported all necessary functions and components
 interface ImageItem {
-    image_resource_url: string;
-  }
-  
-  interface ProviderResponse {
-    status: string;
-    items: ImageItem[];
-  }
-  
-  interface ApiResponse {
-    deepai?: ProviderResponse;
-    // Define other providers as needed
-  }
+  image_resource_url: string;
+}
 
-const GenerateButton: React.FC<GenerateButtonProps> = ({setGeneratedImage, setError, tags, generatedImage, generating, setGenerating }) => {
+interface ProviderResponse {
+  status: string;
+  items: ImageItem[];
+}
 
-  
+interface ApiResponse {
+  deepai?: ProviderResponse;
+  // Define other providers as needed
+}
 
+const GenerateButton: React.FC<GenerateButtonProps> = ({
+  setGeneratedImage,
+  setError,
+  tags,
+  generatedImage,
+  generating,
+  setGenerating,
+}) => {
   const generateImage = async () => {
-    const tagsString = Object.values(tags).join(' ')
+    const tagsString = Object.values(tags).join(" ");
     const options = {
-      method: 'POST',
-      headers: { accept: 'application/json', 'content-type': 'application/json', 'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiMmY3ZDQ3MmEtMGQxNC00YzliLWJjNTYtNzE2ODNkY2M1YThlIiwidHlwZSI6ImFwaV90b2tlbiJ9.ECYgGswnWvSovicXNO_e8Rl53Qt0LwdkPAqw3W89RsE' },
+      method: "POST",
+      headers: {
+        accept: "application/json",
+        "content-type": "application/json",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNDQ4Y2NmM2YtNjRlOC00MTUwLTkwMGUtZDMxOTI5YWM3ZGQ0IiwidHlwZSI6ImFwaV90b2tlbiJ9.EYpWaflnhQHCkl3C6-hwJrvPJ23YP_H2fg4QgThEcCg",
+      },
       body: JSON.stringify({
-        providers:'deepai',
+        providers: "deepai",
         text: tagsString,
         response_as_dict: true,
         attributes_as_list: false,
         show_original_response: false,
-        resolution: '512x512',
-        num_images: 1
-      })
+        resolution: "512x512",
+        num_images: 1,
+      }),
     };
 
-    fetch('https://api.edenai.run/v2/image/generation', options)
-      .then(response => response.json())
+    fetch("https://api.edenai.run/v2/image/generation", options)
+      .then((response) => response.json())
       .then((response: any) => {
         // Assuming the API returns an image URL in the response object
         // Check if the response has the expected property
-        console.log(response)
-        if (response.deepai.status === 'success' && response.deepai.items.length > 0) {
-            const imageUrl = response.deepai.items[0].image_resource_url; // Extract the image URL
-            setGeneratedImage(imageUrl); // Update the image state with the URL
-            setGenerating(false)
+        console.log(response);
+        if (
+          response.deepai.status === "success" &&
+          response.deepai.items.length > 0
+        ) {
+          const imageUrl = response.deepai.items[0].image_resource_url; // Extract the image URL
+          setGeneratedImage(imageUrl); // Update the image state with the URL
+          setGenerating(false);
         } else {
-          console.error('Image data not found in response');
-          setError('Failed to generate image');
+          console.error("Image data not found in response");
+          setError("Failed to generate image");
         }
       })
-      .catch(err => {
+      .catch((err) => {
         console.error(err);
-        setError('Error fetching image');
+        setError("Error fetching image");
       });
   };
 
   // This function should be outside of `generateImage`
   const handleSubmit = () => {
-    if (!tags.tag1 || !tags.tag2 || !tags.tag3){
-      setError('Please give your image three tags!')
+    if (!tags.tag1 || !tags.tag2 || !tags.tag3) {
+      setError("Please give your image three tags!");
     } else {
       generateImage();
       setGenerating(true);
@@ -68,9 +80,13 @@ const GenerateButton: React.FC<GenerateButtonProps> = ({setGeneratedImage, setEr
   };
 
   return (
-    <div className='flex flex-col justify-center'>
-      <button className='py-3 px-6  self-center my-4 bg-cosee-t text-white text-xl rounded-md hover:bg-opacity-90 font-bold'
-        onClick={handleSubmit}>Generate</button>
+    <div className="flex flex-col justify-center">
+      <button
+        className="py-3 px-6  self-center my-4 bg-cosee-t text-white text-xl rounded-md hover:bg-opacity-90 font-bold"
+        onClick={handleSubmit}
+      >
+        Generate
+      </button>
     </div>
   );
 };
